eeabeb52dfb003b8fbe1513faf3664116c5afa97
Stephan <stephanpf@gmail.com>
2018-05-22 13:12:43 +0200

    Add classes Price, ChildrensPrice, NewReleasePrice and RegularPrice

--- src/Movie.java
+++ src/Movie.java
@@ -3,17 +3,33 @@ public class Movie {
     public static final int REGULAR = 0;
     public static final int NEW_RELEASE = 1;
     private String title;
-    private int priceCode;
+    private Price price;
+
     public Movie(String newtitle, int newpriceCode) {
-        title = newtitle;
-        priceCode = newpriceCode;
+        this.title = newtitle;
+        setPriceCode(newpriceCode);
+
+
     }
     public int getPriceCode() {
-        return priceCode;
+        return price.getPriceCode();
     }
     public void setPriceCode(int arg) {
-        priceCode = arg;
+        switch (arg) {
+            case REGULAR:
+                price = new RegularPrice();
+                break;
+            case CHILDRENS:
+                price = new ChildrensPrice();
+                break;
+            case NEW_RELEASE:
+                price = new NewReleasePrice();
+                break;
+            default:
+                throw new IllegalArgumentException("Incorrect Price Code");
+        }
     }
+
     public String getTitle (){
         return title;
     };
