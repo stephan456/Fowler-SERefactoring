e04f9be3e5f747ec4b2219c47e63dae41c52ac51
Stephan <stephanpf@gmail.com>
2018-05-22 12:30:16 +0200

    Move Methodlogic - getFrequentRenterPoints

--- src/Rental.java
+++ src/Rental.java
@@ -31,4 +31,10 @@ class Rental {
         }
         return result;
     }
+    public int getFrequentRenterPoints() {
+        if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) &&  getDaysRented() > 1)
+            return 2;
+        else
+            return 1;
+    }
 }
