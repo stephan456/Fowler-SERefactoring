66b39b76fe779b843462657980d50389c70cc411
Stephan <stephanpf@gmail.com>
2018-05-22 16:06:07 +0200

    Add PriceTest

--- src/main/java/se/fowler/refactoring/model/Price.java
+++ src/main/java/se/fowler/refactoring/model/Price.java
@@ -1,6 +1,6 @@
 package main.java.se.fowler.refactoring.model;
 
-abstract class Price {
+public abstract class Price {
     public abstract int getPriceCode();
 
     public abstract double getCharge(int daysRented);
