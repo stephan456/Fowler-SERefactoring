2bb04b2747dd88d7a0c96df410ae8976c77f8699
Stephan <stephanpf@gmail.com>
2018-05-22 11:26:13 +0200

    Add RentalTest

---
+++ src/test/RentalTest.java
@@ -0,0 +1,24 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RentalTest {
+
+    @Test
+    void getDaysRented() {
+        int daysRented = 2;
+
+        Movie movie = new Movie("Jon Doe in New York", Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie, daysRented);
+
+        assertEquals(daysRented, rental.getDaysRented());
+    }
+
+    @Test
+    void getMovie() {
+        Movie movie = new Movie("Jon Doe in New York", Movie.NEW_RELEASE);
+        Rental rental = new Rental(movie, 0);
+
+        assertEquals(movie, rental.getMovie());
+    }
+}
